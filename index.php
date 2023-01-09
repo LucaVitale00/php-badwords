9ac3cb3ae76db9bb824b0fef687d5bed23de6da2
LucaVitale00 <luca16vitale2000@gmail.com>
2023-01-09 17:22:41 +0100

    aggiungo censura e lunghezza testo

--- index.php
+++ index.php
@@ -1,12 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Php Badwords</title>
 </head>
+
 <body>
-    
+    <h1>
+        <?php
+        $title = "Lorem Ipsum PHP";
+        echo $title;
+        ?>
+    </h1>
+
+    <p>
+        <?php
+        $LoremIpsum = "Lorem ipsum dolor sit amet. Sit nobis recusandae sed maxime voluptatum aut laboriosam voluptatem vel ratione perferendis sit tenetur sunt. Qui deleniti rerum est facilis rerum eum doloribus            exercitationem est nihil pariatur? Cum magnam nostrum ut galisum quam sit exercitationem aliquam vel quas vitae.";
+        echo $LoremIpsum;
+        ?>
+    </p>
+
+    <span>
+        <?php
+        echo "La lunghezza del testo è di " . strlen($LoremIpsum) . " caratteri";
+        ?>
+    </span>
+
+    <p>
+        <?php
+        $censura = $_GET['parola'];
+        $NewLorem = str_replace($censura, "***", $LoremIpsum);
+        echo $NewLorem;
+        ?>
+    </p>
+
+    <span>
+        <?php
+        echo "La lunghezza del testo è di " . strlen($NewLorem) . " caratteri";
+        ?>
+    </span>
 </body>
+
 </html>
